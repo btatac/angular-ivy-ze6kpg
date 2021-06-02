803cbf84fcf24f5e1eb4d363762ef0aeb4ef40da
Hellen Barbosa <hellenbarbosa@Braejans-MacBook-Air.local>
2021-06-02 17:18:38 -0500

    Clase 2 de junio 2021 estilos

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,27 +1,47 @@
-import {Component,Input,Output,EventEmitter,OnChanges, SimpleChanges, OnInit } from '@angular/core';
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges, 
+    SimpleChanges, 
+    OnInit,
+    DoCheck, 
+    OnDestroy
+} from '@angular/core';
 
 import {Product} from '../product.model';
 
 @Component({
     selector : 'app-product',
-    templateUrl : './product.component.html'
+    templateUrl : './product.component.html',
+    styleUrls : ['./product.components.scss']
 })
-export class ProductComponent implements OnChanges, OnInit{
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any>= new EventEmitter();
 
+  //today = new Date();
 constructor() {
     console.log('1.constructor');
 }
 
-ngOnChanges (changes: SimpleChanges) {
-    console.log('2.ngOnchanges');
-    console.log(changes);
-}
+//ngOnChanges (changes: SimpleChanges) {
+  //  console.log('2.ngOnchanges');
+   // console.log(changes);
+//}
 
 ngOnInit()  {
     console.log('3.ngOnInit');
+}
+
+ngDoCheck(){
+    console.log('4.ngDocheck');
+}
+
+ngOnDestroy (){
+    console.log('5.ngOnDestroy');
 }
 
   addCart() {
