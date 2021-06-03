21cc63b8f4bfc29a5edf62dc53e7b75e0ded9702
Hellen Barbosa <hellenbarbosa@Braejans-MacBook-Air.local>
2021-06-03 17:42:11 -0500

    clases del 3 julio 2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,38 @@ import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 import {ProductComponent} from './components/product.component';
 import { CartComponent } from './cart/cart.component';
+import { ExponentialPipe } from './exponential.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { SwiperModule } from 'swiper/angular';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    CartComponent
+    CartComponent,
+    ExponentialPipe,
+    HighlightDirective,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
